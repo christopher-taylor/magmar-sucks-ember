d2c5a1f76cd5d3a64f74f6a3e027912433207b22
Chris Taylor <me@christaylor.zone>
2017-04-23 12:51:20 -0700

    Mobile nav now closes when link is clicked

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,26 +1,28 @@
 import Ember from 'ember';
-
 export default Ember.Controller.extend({
   remodal: Ember.inject.service(),
   address: undefined,
   subject: undefined,
   body: undefined,
-  sendDisabled: Ember.computed('address', 'subject', 'body', function(){
+  sendDisabled: Ember.computed('address',
+    'subject', 'body',
+    function() {
       console.log({
-          bodyVal: this.get('body'),
-          addressVal: this.get('address'),
-
-      })
-    return (!this.get('address') || !this.get('subject') || !this.get('body'));
-  }),
-
+        bodyVal: this.get('body'),
+        addressVal: this.get('address'),
+      });
+       return (!this.get('address') || !this.get('subject') || !this.get('body'));
+    }),
   closeModal() {
     this.get('remodal').close();
   },
-
   actions: {
     openModal() {
       this.get('remodal').open();
+    },
+    toggleAndTransition(routeName) {
+      Ember.$('.collapse').collapse('hide');
+      this.transitionToRoute(routeName);
     }
   }
 });
