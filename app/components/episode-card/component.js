2b435c63d56e8314d96e8bdfc7a39181f26b20cc
Chris Taylor <me@christaylor.zone>
2017-05-26 09:12:01 -0700

    Clean up classes and markup, change header

--- app/components/episode-card/component.js
+++ app/components/episode-card/component.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNames: ['row', 'mb-3', 'episode-card-wrapper'],
-    classNameBindings: ['media.isMobile:ml-1', 'media.isMobile:mr-1']
+    classNames: ['row', 'mb-3', 'episode-card-wrapper']
 });
