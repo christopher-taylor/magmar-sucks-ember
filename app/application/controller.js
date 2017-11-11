83127afb0115044b2807cd649b1550e64c2a7074
Christopher Taylor <me@christaylor.zone>
2017-11-11 06:13:47 -0800

    Update some dependencies and tweak code

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,30 +1,33 @@
 import $ from 'jquery';
-import { inject as service } from '@ember/service';
+import {
+  inject as service
+} from '@ember/service';
 import Controller from '@ember/controller';
-import computed from 'ember-computed-decorators';
+import {
+  computed,
+  action
+} from 'ember-decorators/object';
 
 export default Controller.extend({
   remodal: service(),
-  address: undefined,
-  subject: undefined,
-  body: undefined,
 
   @computed('address', 'subject', 'body')
-  sendDisabled(address, subject, body) {
-     return (!address || !subject || !body);
+  get sendDisabled() {
+    return (!this.get('address') || !this.get('subject') || !this.get('body'));
   },
 
   closeModal() {
     this.get('remodal').close();
   },
 
-  actions: {
-    openModal() {
-      this.get('remodal').open()
-    },
-    toggleAndTransition(routeName) {
-      $('.collapse').collapse('hide');
-      this.transitionToRoute(routeName);
-    }
+  @action
+  openModal() {
+    this.get('remodal').open()
+  },
+  @action
+  toggleAndTransition(routeName) {
+    $('.collapse').collapse('hide');
+    this.transitionToRoute(routeName);
+
   }
 });
