6dd1c5e7d7487b80d3acd941c622b3e6d320e1cb
Christopher Taylor <me@christaylor.zone>
2017-12-02 07:52:20 -0800

    No idea what changed

--- app/email/controller.js
+++ app/email/controller.js
@@ -4,10 +4,6 @@ import {
 } from 'ember-decorators/object';
 
 export default Controller.extend({
-  address: "",
-  subject: "",
-  body: "",
-
   @computed('address', 'subject', 'body')
   get sendDisabled() {
     return (!this.get('address') || !this.get('subject') || !this.get('body'));
