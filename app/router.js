bda0c7a79424fa2f63d94cc791dd4a487789ef7f
Christopher Taylor <me@christaylor.zone>
2018-03-03 14:53:17 -0800

    Use hash location style

--- app/router.js
+++ app/router.js
@@ -2,8 +2,8 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+  location: 'hash',
+  rootURL: '/'
 });
 
 Router.map(function() {
