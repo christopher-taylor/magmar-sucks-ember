39139e97703f94fc05919afc8f00312018651275
Chris Taylor <me@christaylor.zone>
2018-10-07 22:03:17 -0700

    fix main page. update rankings

--- app/episode/adapter.js
+++ app/episode/adapter.js
@@ -1,10 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-    findAll(store, type, sinceToken, snapshotRecordArray) {
-        return $.getJSON("https://magmarsucks.fireside.fm/json", function (result) {
-          //response data are now in the result variable
-          return result['items'];
-        });;
-    }
+  findAll(store, type, sinceToken, snapshotRecordArray) {
+    return $.getJSON("https://cors.io/?https://magmarsucks.fireside.fm/json", function (result) {
+        console.log(result);
+        return result["items"];
+      //response data are now in the result variable
+    });
+  },
+
 });
